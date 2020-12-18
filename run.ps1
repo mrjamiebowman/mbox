da5732104db69798746fc6e841e838248bc42344
Jamie Bowman <jamie.bowman@csiweb.com>
2020-12-18 11:13:52 -0600

    use host..fixes kubens

--- run.ps1
+++ run.ps1
@@ -1,1 +1,1 @@
-docker run -it --rm --name mbox -v mbox:/mbox -v work:/work -v $env:userprofile/.kube:/root/.kube -v //var/run/docker.sock:/var/run/docker.sock mrjamiebowman/mbox /bin/bash
+docker run -it --rm --name mbox --network host -v mbox:/mbox -v work:/work -v $env:userprofile/.kube:/root/.kube -v //var/run/docker.sock:/var/run/docker.sock mrjamiebowman/mbox /bin/bash

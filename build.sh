07245ccb9b649cf035dbb5900cd52d93dcdb23a4
Jamie Bowman <mrjamiebowman@gmail.com>
2020-12-19 12:06:23 -0500

    comments on travis build

--- build.sh
+++ build.sh
@@ -7,14 +7,16 @@
 # GITHUB_TOKEN
 
 image="mrjamiebowman/mbox"
+
 LATEST_TAG=$(git ls-remote --tags origin |awk -F \/ '{print $NF}'|grep ^1.0. |sort -Vr|head -1)
+echo "Latest Tag: $LATEST_TAG"
 
 build() {
   docker build --no-cache -t ${image}:${tag} .
 }
 
 status=$(curl -sL https://hub.docker.com/v2/repositories/${image}/tags/${tag})
-echo $status
+echo "Status: $status"
 
 if [[ ( "${status}" =~ "not found" ) || ( ${REBUILD} == "true" ) ]]; then
     build
